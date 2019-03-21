edbc57e1f2879251a08566f34ba8dc27bfdd336a
bastazini <bastazini.vinicius@gmail.com>
2019-03-21 14:18:00 +0100

    Update grraf.R

--- grraf.R
+++ grraf.R
@@ -14,14 +14,12 @@ par(mfrow=c(1,2))
 
 ##Beta diversity
 x=c(1,2,3,4)
-
-avg=c()
-lower=c()
-upper=c()
+avg=c(-0.0043,0.0563,0.0481,-0.0157)
+lower=c(-0.0716, 0.0155 , -0.0229, -0.0466)
+upper=c(0.0631,0.0970,  0.1191,  0.0153)
 plot(1, type="n", xlab="", xaxt="n", ylab="Effect size (95%IC)", xlim=c(0.5, 4.5), ylim=c(-.2, .2))
 points(x,avg, pch=16)
 arrows(x, lower, x, upper, length=0.05, angle=90, code=3)
 abline(h=0,lty=2, lwd=2)
 axis(side=1,at=x,label=c("Nestedness"," Turnover"," Gradient abundance","Balanced abundance"), las=1)
-
 
